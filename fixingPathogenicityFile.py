bb5eb91ca861d4025c1b2de62453e78bca81c549
cazcullimore <cazcullimore@gmail.com>
2021-06-18 10:17:50 -0600

    I think this program is working properly right now

--- fixingPathogenicityFile.py
+++ fixingPathogenicityFile.py
@@ -3,12 +3,33 @@
 
 pathogenicityOnlyFilePath = "/Users/cazcullimore/Documents/ericksonLabCode/megaCATS-main/firstMegaCatsRunOnData/pathogenicity-rMsaInput.txt-rResultMGCStat.txt"
 # can copy into all file
-fileToFixPath = "Users/cazcullimore/Documents/ericksonLabCode/megaCATS-main/firstMegaCatsRunOnData/pathogenicity-rMsaInput.txt-rResultChisqTest.txt" # modified
+fileToFixPath = "/Users/cazcullimore/Documents/ericksonLabCode/megaCATS-main/firstMegaCatsRunOnData/pathogenicity-rMsaInput.txt-rResultChisqTest.txt" # modified
 
 readData = []
 with open(fileToFixPath) as file:
     for line in file:
         readData.append(line)
+print(len(readData))
+currIndex = 0
+with open(pathogenicityOnlyFilePath) as pathOnlyFile, open(fileToFixPath + "Test", "w") as fixFile:
+    for pathOnlyLine in pathOnlyFile:
+        pathOnlyLine = pathOnlyLine.strip()
+        cols = pathOnlyLine.split("\t")
+        if len(cols) <= 2 or pathOnlyLine[0] == '"' or cols[2].strip() != "2" or cols[3].strip() != "3":
+            # print("skipped")
+            continue
+        pathOnlySnpIndex = cols[0] # string
+        correctPval = cols[1]
+        # print("parth index", pathOnlySnpIndex)
+        # try:
+        for fixLine in readData[currIndex:]:
+            fixCols = fixLine.split("\t")
+            # print("other inde", fixCols[0])
+            currIndex += 1
+            if fixCols[0].strip() == pathOnlySnpIndex.strip():
+                fixFile.write(pathOnlySnpIndex + "\t" + fixCols[1] + "\t"+ correctPval + "\t" + "\t".join(fixCols[3:])) # TODO: check
+                break
 
-with open(pathogenicityOnlyFilePath) as pathOnlyFile, open(fileToFixPath, "w") as fixFile:
-    for line in pathOnlyFile:
+        # except:
+        #     pass
+
