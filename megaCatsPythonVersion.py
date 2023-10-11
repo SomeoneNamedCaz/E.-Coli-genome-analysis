d48c2d0daa9cf50f554f1a36f40258bd9b894571
cazcullimore <cazcullimore@gmail.com>
2023-10-10 23:59:01 -0600

    changed it to lists of nucs, not tested

--- megaCatsPythonVersion.py
+++ megaCatsPythonVersion.py
@@ -7,15 +7,18 @@ import numpy as np
 alignedFilePath = sys.argv[1]
 metadataFilePath = sys.argv[2]
 
+charToIndex = {"A":0, "T":1, "C":2, "G":3, "-": 4, "N":5}
 class NucCounter():
 
-    def __init__(self,nameOfGroup = "unnamed", counts=[]):
-        self._nucCounts = counts
+    def __init__(self,countLength = 0, nameOfGroup = "unnamed",):
+        self.nucCounts = []
+        for _ in range(countLength):
+            self.nucCounts.append([0] * len(charToIndex.values()))
         self.nameOfGroup = nameOfGroup
     def getDistributionAt(self, positionInGenome):
-        return self._nucCounts[positionInGenome]
+        return self.nucCounts[positionInGenome]
     def appendDistribution(self, distribution):
-        self._nucCounts.append(distribution)
+        self.nucCounts.append(distribution)
 
 possibleClassValues = [] # maybe do a separate class for this
 seqNameToMetaDataType = {}
@@ -37,28 +40,40 @@ with open(metadataFilePath) as metadataFile: # process metadata
                 #     possibleClassValues[colIndex] = {}
 
 print(possibleClassValues)
+
+def makeNucCounters(lengthOfSeqs, optionsForMetadataCategories):
+    dictOfNucCounters = {}
+    for val1 in optionsForMetadataCategories[0]:
+        dictOfNucCounters[val1] = NucCounter(nameOfGroup=val1, countLength=lengthOfSeqs)
+        # for val2 in optionsForMetadataCategories[1]:
+        #     dictOfNucCounters[val1+val2] = NucCounter(nameOfGroup=val1+val2)
+
+            # # TODO: can have conflict if optionsForMetadataCategories[0] and optionsForMetadataCategories[1] have same values
+    for val2 in optionsForMetadataCategories[1]:
+        dictOfNucCounters[val2] = NucCounter(nameOfGroup=val2, countLength=lengthOfSeqs)
+    return dictOfNucCounters
+
 nucCounters = {}
 # maybe better with a matrix but 2 minutes isn't that bad
+# isPathogenic = np.array()
+# isCow = np.array()
 
 with open(alignedFilePath) as alignedFile:
     # for metaDataTypes in range(len(possibleClassValues)):
     #     nucCounters =
     # hard code for now
 
-    for val1 in possibleClassValues[0]:
-        nucCounters[val1] = NucCounter(nameOfGroup=val1)
-        # for val2 in possibleClassValues[1]:
-        #     nucCounters[val1+val2] = NucCounter(nameOfGroup=val1+val2)
 
-            # # TODO: can have conflict if possibleClassValues[0] and possibleClassValues[1] have same values
-    for val2 in possibleClassValues[1]:
-        nucCounters[val2] = NucCounter(nameOfGroup=val2)
     nameOfSeq = "unknown"
+    isFirstDataLine = True
     for line in alignedFile:
         line = line.strip()
         if line[0] == ">":
             nameOfSeq = line[1:]
         else:
+            if isFirstDataLine:
+                isFirstDataLine = False
+                nucCounters = makeNucCounters(len(line), possibleClassValues)
             # nameOfNucCounter = "".join(seqNameToMetaDataType[nameOfSeq])
 
             for i in range(len(possibleClassValues)):
@@ -66,12 +81,7 @@ with open(alignedFilePath) as alignedFile:
                 charIndex = -1
                 for char in line:
                     charIndex += 1
-                    try:
-                        nucCounters[nameOfNucCounter]._nucCounts[charIndex][char] += 1
-                    except KeyError:
-                        nucCounters[nameOfNucCounter]._nucCounts[charIndex][char] = 1
-                    except IndexError:
-                        nucCounters[nameOfNucCounter]._nucCounts.append({char:1})
+                    nucCounters[nameOfNucCounter].nucCounts[charIndex][charToIndex[char]] += 1
 
 
 for metaDataCategoryIndex in range(len(possibleClassValues)):
@@ -81,12 +91,10 @@ for metaDataCategoryIndex in range(len(possibleClassValues)):
         # for
         nucCountersOfOneMetadataCatagory.append(nucCounters[option])
     # TODO: only works with two options in each metadatacatagory
-    nucIndex = 0
-    while True:
-        try:
-            dist1 = nucCountersOfOneMetadataCatagory[0].getDistributionAt([nucIndex])
-            dist2 = nucCountersOfOneMetadataCatagory[1].getDistributionAt([nucIndex])
-            for value in range(max(len(dist1.values), len(dist2.values))):
-
-        except IndexError:
-            break
+    for nucIndex in range(len(nucCountersOfOneMetadataCatagory[0].nucCounts)):
+        dist1 = nucCountersOfOneMetadataCatagory[0].getDistributionAt([nucIndex])
+        dist2 = nucCountersOfOneMetadataCatagory[1].getDistributionAt([nucIndex])
+        dist2 = np.array(dist2)
+        dist1 = np.array(dist1)
+        degOfFreedom = max(sum([val != 0 for val in dist1]), sum([val != 0 for val in dist2]))
+        print(dist1, dist2, "chisquare P value??",stats.chi2.cdf(sum((dist1-dist2)**2/dist1)),df=degOfFreedom)
